247213de53442796560d8110e1acae4d47dfdb70
YeffersonDelgado <yeffersondaviddega@ufps.edu.co>
2023-10-09 14:02:30 -0500

    cuarta prueba

--- ArchivosJS/tVista.js
+++ ArchivosJS/tVista.js
@@ -1,39 +1,31 @@
 document.getElementById("btn-6").addEventListener("click", function() {
-    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
-    window.location.href = "index.html"; // Reemplaza "otra-vista.html" con la URL deseada
+    window.location.href = "../index.html";
 });
 
 document.getElementById("btn-10").addEventListener("click", function() {
-    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
-    window.location.href = "otro_valor.html"; // Reemplaza "otra-vista.html" con la URL deseada
+    window.location.href = "otro_valor.html"; 
 });
 
 document.getElementById("btn-4").addEventListener("click", function() {
-    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
-    window.location.href = "resultado_retiro.html"; // Reemplaza "otra-vista.html" con la URL deseada
+    window.location.href = "resultado_retiro.html"; 
 });
 
 document.getElementById("btn-5").addEventListener("click", function() {
-    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
-    window.location.href = "resultado_retiro.html"; // Reemplaza "otra-vista.html" con la URL deseada
+    window.location.href = "resultado_retiro.html"; 
 });
 
 document.getElementById("btn-6").addEventListener("click", function() {
-    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
-    window.location.href = "resultado_retiro.html"; // Reemplaza "otra-vista.html" con la URL deseada
+    window.location.href = "resultado_retiro.html";
 });
 
 document.getElementById("btn-7").addEventListener("click", function() {
-    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
-    window.location.href = "resultado_retiro.html"; // Reemplaza "otra-vista.html" con la URL deseada
+    window.location.href = "resultado_retiro.html";
 });
 
 document.getElementById("btn-8").addEventListener("click", function() {
-    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
-    window.location.href = "resultado_retiro.html"; // Reemplaza "otra-vista.html" con la URL deseada
+    window.location.href = "resultado_retiro.html";
 });
 
 document.getElementById("btn-9").addEventListener("click", function() {
-    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
-    window.location.href = "resultado_retiro.html"; // Reemplaza "otra-vista.html" con la URL deseada
+    window.location.href = "resultado_retiro.html"; 
 });
