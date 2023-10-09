f3045aa8aa736c882176182910468145943f88a1
YeffersonDelgado <yeffersondaviddega@ufps.edu.co>
2023-10-09 15:42:59 -0500

    ultimo cambio

--- ArchivosJS/sVista.js
+++ ArchivosJS/sVista.js
@@ -12,3 +12,8 @@ document.getElementById("btn-5").addEventListener("click", function() {
     // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
     window.location.href = "../index.html"; // Reemplaza "otra-vista.html" con la URL deseada
 });
+
+document.getElementById("btn-2").addEventListener("click", function() {
+    // Coloca la URL de la otra vista o página web que deseas abrir en el siguiente enlace:
+    window.location.href = "clave.html"; // Reemplaza "otra-vista.html" con la URL deseada
+});
